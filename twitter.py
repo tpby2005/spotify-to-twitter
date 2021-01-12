4d708e7cceeb334c06bc72481e868575a6a4aca7
delinqu3nt <toby.bartholovitsch@gmail.com>
2021-01-12 14:32:37 -0800

    working, infinite closed loop issue

--- twitter.py
+++ twitter.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import time
 from spotify import getwindow
 from processid import getSong
+from SwSpotify import SpotifyClosed, SpotifyNotRunning, SpotifyPaused
 
 consumer_key = ""
 consumer_secret = ""
@@ -36,12 +37,46 @@ secret = auth.access_token_secret
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(key, secret)
-
+bio = "value"
+bio1 = "value1"
+closed = True
 
 while True:
-    bio = getSong()
-    print(bio)
-    api.update_profile(
-        description = bio
-    )
     time.sleep(5)
+    try:
+        bio = getSong()
+        while bio != bio1:
+            print(bio)
+            api.update_profile(
+                description = bio
+            )
+            bio1 = bio
+        else:
+            continue
+    except SpotifyClosed as error:
+        print('spotify closed')
+        if closed == False:
+            api.update_profile(
+                description = "Spotify Closed"
+            )
+            closed = True
+        elif closed == True:
+            continue
+    except SpotifyPaused as error:
+        print('spotify paused')
+        if closed == False:
+            api.update_profile(
+                description = "Spotify Paused"
+            )
+            closed = True
+        elif closed == True:
+            continue
+    except SpotifyNotRunning as error:
+        print('spotify not running')
+        if closed == False:
+            api.update_profile(
+                description = "Spotify Not Running"
+            )
+            closed = True
+        elif closed == True:
+            continue
