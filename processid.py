85f40435b1fa8440322f3db5876ae50451e32bf9
delinqu3nt <toby.bartholovitsch@gmail.com>
2021-01-13 11:08:18 -0800

    credits added

--- processid.py
+++ processid.py
@@ -3,5 +3,5 @@ from SwSpotify import spotify
 def getSong():
     song = spotify.song()
     artist = spotify.artist()
-    bio = "I am currently listening to: \n" + song + " \n" + artist
+    bio = "I am currently listening to: \n" + song + " \n" + artist + "\nhttps://github.com/tpby2005/spotify-to-twitter"
     return bio
